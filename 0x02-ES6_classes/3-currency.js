6d0f3b1b990be6c74f2c3e870e58061d92e041ce
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2024-01-04 11:08:05 +0200

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,25 +1,9 @@
 export default class Currency {
   constructor(code, name) {
-    (this._code = code), (this._name = name);
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(value) {
-    this._code = value;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    this._name = value;
+    this._code = code;
+    this._name = name;
   }
 
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return (`${this.name} (${this.code})`);
   }
-}
